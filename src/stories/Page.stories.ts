42a92800527d0227cfaa12c23ba30ff8db56b01b
Michael Shilman <shilman@users.noreply.github.com>
2026-01-18 12:51:22 +0800

    Convert play to test function

--- src/stories/Page.stories.ts
+++ src/stories/Page.stories.ts
@@ -15,14 +15,24 @@ export const LoggedOut = meta.story();
 
 // More on component testing: https://storybook.js.org/docs/writing-tests/interaction-testing
 export const LoggedIn = meta.story({
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
+  play: async ({ canvas }) => {
     const loginButton = canvas.getByRole('button', { name: /Log in/i });
     await expect(loginButton).toBeInTheDocument();
     await userEvent.click(loginButton);
-    await expect(loginButton).not.toBeInTheDocument();
-
-    const logoutButton = canvas.getByRole('button', { name: /Log out/i });
-    await expect(logoutButton).toBeInTheDocument();
   },
+});
+
+LoggedIn.test('it should be logged in', async ({ canvas }) => {
+  const logoutButton = canvas.getByRole('button', { name: /Log out/i });
+  await expect(logoutButton).toBeInTheDocument();
+});
+
+LoggedIn.test('it should log out', async ({ canvas }) => {
+  const logoutButton = await canvas.findByRole('button', { name: /Log out/i });
+  await expect(logoutButton).toBeInTheDocument();
+
+  await userEvent.click(logoutButton);
+
+  const loginButton = canvas.getByRole('button', { name: /Log in/i });
+  await expect(loginButton).toBeInTheDocument();
 });
